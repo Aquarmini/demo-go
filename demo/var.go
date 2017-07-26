0a088c6d991d3f7e7fa0828c8dc2531cece4317c
李铭昕 <715557344@qq.com>
2017-07-26 13:05:45 +0800

    *

--- demo/var.go
+++ demo/var.go
@@ -1,8 +1,30 @@
 package main
-var a = "w3cschool菜鸟教程"
+
+var a = "w3cschool菜鸟教程";
+
 var b string = "w3cschool.cc"
 var c bool
+var num int = 0;
 
-func main(){
-    println(a, b, c)
+var arr [10] int;
+
+func main() {
+	println(a, b, c)
+
+	for j := 0; j <= 10000; j++ {
+		for i := 0; i <= 10000; i++ {
+			num++;
+		}
+	}
+	println(num);
+
+	arr1 := []int{1, 2, 3};
+	for _, v := range arr1 {
+		println(v);
+	}
+
+	arr1 = append(arr1, 5);
+	for _, v := range arr1 {
+		println(v);
+	}
 }
